4bbc5d089314ef431afbb7dcaba13a283656c7cc
unknown <2314290086@academico.iesb.br>
2025-02-25 19:20:03 -0300

    aula01 - terminando os commits

---
+++ aulas/aula01/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{flex: 1, backgroundColor: "blue"}}>
+      <MeuComponente></MeuComponente>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function MeuComponente() {
+  return (
+    <Text style={{paddingTop: 24, fontSize: 24, color: "white"}}>Olá, Mundo!</Text>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
