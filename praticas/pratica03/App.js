a11362b16b53be7877ec0145542f72fd6be78e7f
unknown <2314290086@ACADEMICO.IESB.BR>
2025-03-14 21:07:02 -0300

    atualizando pratica03

--- praticas/pratica03/App.js
+++ praticas/pratica03/App.js
@@ -1,6 +1,7 @@
 import ListScreen from "./screens/ListScreen";
 import GalleryScreen from "./screens/GaleryScreen";
 import HomeScreen from "./screens/HomeScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 function App() {
   return <ListScreen></ListScreen>
 }
