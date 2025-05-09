4b2ab91cdd2730608062559d1b1d5889e7fb9ad7
unknown <2314290086@ACADEMICO.IESB.BR>
2025-05-09 20:30:56 -0300

    Continuando aula07 (2/3)

--- aulas/aula07/screens/Login.js
+++ aulas/aula07/screens/Login.js
@@ -1,9 +1,51 @@
-
+import { View } from "react-native";
+import { TextInput, HelperText, Button } from "react-native-paper";
+import { useForm, Controller } from "react-hook-form";
 
 function Login() {
-    return (
-        <></>
-    )
+  const { control } = useForm();
+  return (
+    <View style={{ flex: 1, padding: 16 }}>
+      <Controller
+        control={control}
+        name="email"
+        render={({ field: {value, onChange} }) => (
+          <TextInput
+            label="E-mail"
+            mode="outlined"
+            keyboardType="email-adress"
+            autoCapitalize="none"
+            value={value}
+            onChangeText={onChange}
+          />
+        )}
+      />
+      <HelperText type="error" visible={false}></HelperText>
+      <Controller
+        control={control}
+        name="senha"
+        render={({ field: {value, onChange} }) => (
+          <TextInput
+            label="Senha"
+            mode="outlined"
+            secureTextEntry
+            value={value}
+            onChangeText={onChange}
+          />
+        )}
+      />
+      <HelperText type="error" visible={false}></HelperText>
+      <Button mode="contained" onPress={() => {}} style={{ marginBottom: 16 }}>
+        Entrar
+      </Button>
+      <Button mode="outlined" onPress={() => {}} style={{ marginBottom: 16 }}>
+        Criar Conta
+      </Button>
+      <Button mode="outlined" onPress={() => {}}>
+        Redefinir Senha
+      </Button>
+    </View>
+  );
 }
 
 export default Login;
