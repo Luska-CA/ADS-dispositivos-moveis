722f87d51326d9cd41473c64a83a8c0d6e5c7d43
unknown <2314290086@academico.iesb.br>
2025-06-06 20:32:01 -0300

    metade da pratica11

---
+++ praticas/pratica11/components/TaskModal.js
@@ -0,0 +1,9 @@
+import { useContext, useState } from "react";
+import { View } from "react-native";
+import { Button, TextInput } from "react-native-paper";
+
+function TaskModal({setShowModal}) {
+    
+}
+
+export default TaskModal;
